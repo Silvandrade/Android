cc3895b2fc612e6764f4e80c0ca2bcfcf84479bf
Silvandrade <diegodsa13@gmail.com>
2021-10-09 14:32:10 -0300

    Button Customizado
    Trabalhando com declaração de atributos styleable e customizando componentes para embutir ProgressBar no Button a partir do um FrameLayout.

---
+++ Instagram/app/src/main/java/co/tiagoaguiar/course/instagram/login/view/LoadingButton.kt
@@ -0,0 +1,63 @@
+package co.tiagoaguiar.course.instagram.login.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
+import android.widget.FrameLayout
+import android.widget.ProgressBar
+import co.tiagoaguiar.course.instagram.R
+
+class LoadingButton : FrameLayout {
+
+    private lateinit var button: Button
+    private lateinit var progress: ProgressBar;
+    private lateinit var text: String
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+        setup(context, attrs)
+    }
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    private fun setup(context: Context, attrs: AttributeSet?) {
+        // Inflando o layout xml dentro do LoadingButton/Frame Layout.
+        val inflater: LayoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        inflater.inflate(R.layout.button_loading, this)
+
+        // Lista de atributos criados no arquivo attrs.xml
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.LoadingButton, 0, 0)
+        text = typedArray.getString(R.styleable.LoadingButton_text).toString() // Recuperando valor definido no atributo text criano no attrs.xml
+
+        button = this.getChildAt(0) as Button // Pegando o botão criado no nosso button_loading.xml
+        progress = this.getChildAt(1) as ProgressBar // Pegando a ProgressBar criada no nosso button_loading.xml
+
+        button.text = text
+        button.isEnabled = false
+
+        typedArray.recycle() // Para diminuir a lista de array.
+    }
+
+    override fun setEnabled(enabled: Boolean) { // Sobrescrevendo o metodo para converter a propriedade do FrameLayout para a propriedade do botão.
+        button.isEnabled = enabled
+    }
+
+    override fun setOnClickListener(l: OnClickListener?) { // Sobrescrevendo o metodo para converter o clique do FrameLayout no clique do botão.
+        button.setOnClickListener(l) // Passando o listener para nosso botão.
+    }
+
+    public fun showProgress(enabled: Boolean) {
+        if(enabled) {
+            button.text = ""
+            button.isEnabled = false
+            progress.visibility = View.VISIBLE
+        } else {
+            button.text = text
+            button.isEnabled = true
+            progress.visibility = View.GONE
+        }
+    }
+}
--- Instagram/app/src/main/java/co/tiagoaguiar/course/instagram/login/view/LoginActivity.kt
+++ Instagram/app/src/main/java/co/tiagoaguiar/course/instagram/login/view/LoginActivity.kt
@@ -1,12 +1,17 @@
-package co.tiagoaguiar.course.instagram
+package co.tiagoaguiar.course.instagram.login.view
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
+import co.tiagoaguiar.course.instagram.R
 import com.google.android.material.textfield.TextInputLayout
 
 class LoginActivity : AppCompatActivity() {
@@ -16,18 +21,26 @@ class LoginActivity : AppCompatActivity() {
 
     val editTextEmail = findViewById<EditText>(R.id.login_edit_email)
     val editTextPassword = findViewById<EditText>(R.id.login_edit_password)
-    val buttonEnter = findViewById<Button>(R.id.login_btn_enter)
+    val buttonEnter = findViewById<LoadingButton>(R.id.login_btn_enter)
 
     editTextEmail.addTextChangedListener(watcher) // Evento de escuta de alteração de texto.
     editTextPassword.addTextChangedListener(watcher)
 
-    // Objeto anônimo que implementa uma interface.
+    // Objeto anônimo que implementa uma interface, quando o botão é clicado é acionado o metodo Listener do LoadingButton que é um FrameLayout por extensão.
     buttonEnter.setOnClickListener{
+
+      closeKeyboard(this)
+      buttonEnter.showProgress(true);
+
       findViewById<TextInputLayout>(R.id.login_edit_email_input)
         .error = "Esse e-mail é inválido."
 
       findViewById<TextInputLayout>(R.id.login_edit_password_input)
         .error = "Senha inválida."
+
+      Handler(Looper.getMainLooper()).postDelayed({ // Método para simular um atraso de 2 segundo.
+        buttonEnter.showProgress(false)
+      }, 2000)
     }
   }
 
@@ -36,12 +49,18 @@ class LoginActivity : AppCompatActivity() {
     }
 
     override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) { // Toda vez que o texto for alterado.
-      val btnEnter = findViewById<Button>(R.id.login_btn_enter)
+      val btnEnter = findViewById<LoadingButton>(R.id.login_btn_enter)
       btnEnter.isEnabled = s.toString().isNotEmpty() // Retorna um boolean de edição de texto do campo e-mail.
     }
 
     override fun afterTextChanged(s: Editable?) {
     }
 
+  }
+
+  private fun closeKeyboard(activity: LoginActivity) {
+    val inputMethodManager: InputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    val view: View? = activity.currentFocus
+    inputMethodManager.hideSoftInputFromWindow(view?.windowToken, 0)
   }
 }
